0e1989fffe11eadafc63cd7e11822277dc7f4629
Majirefy <song.fc@outlook.com>
2020-01-08 21:17:43 +0800

    Initial Commit

---
+++ AmourX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmourX
+//
+//  Created by Fangcheng Song on 2020/1/8.
+//  Copyright © 2020 Fangcheng Song. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
